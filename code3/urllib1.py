386e582ce06f12b098580f9a77b189d94f45cd8e
justingfoster <justingfoster@hotmail.com>
2017-07-19 08:11:41 -0400

    7/18 v3

--- code3/urllib1.py
+++ code3/urllib1.py
@@ -1,6 +1,5 @@
-import urllib.request
+import urllib.request, urllib.parse, urllib.error
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
     print(line.decode().strip())
-testing htis
